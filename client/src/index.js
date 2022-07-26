f516ff9469b4ef2c5c9286d522260833c4c389a6
Alan <alangiavino97@gmail.com>
2022-07-26 01:20:44 -0300

    finish deploy client

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,6 @@ import "./index.css";
 import App from "./App";
 import { store } from "./store";
 import axios from 'axios';
-import dotenv from "dotenv";
-dotenv.config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 const root = ReactDOM.createRoot(document.getElementById("root"));
